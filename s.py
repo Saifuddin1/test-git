5f08d0e655f7ff02904ff5cb39b8befebd6d07f2
mohammad saifuddin <Mohammad.Saifuddin_ext@ltts.com>
2023-09-26 16:31:47 +0530

    Revert "modify the file at 4.24"
    This reverts commit e9b86b606e597d897bcbf378a8179f7d5095e96b.

--- s.py
+++ s.py
@@ -1,4 +1,3 @@
 print("This si my second file which i am going to add in main branch")
 print("made some modification in the second file")
 print("What are you doing man")
-print("My name is isaifuddin")
