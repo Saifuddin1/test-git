35fbbaa456bd97d252087d50794118cc5aa43093
mohammad saifuddin <Mohammad.Saifuddin_ext@ltts.com>
2023-09-26 16:19:52 +0530

    Revert "modify the file"
    This reverts commit 1fd28d4215e2f49362c0157aa3c82a88413fb55b.

--- s.py
+++ s.py
@@ -1,6 +1,3 @@
 print("This si my second file which i am going to add in main branch")
 print("made some modification in the second file")
 print("What are you doing man")
-print(
-    "No i have to switch to another brnach as i have to make some changes in main branch"
-)
